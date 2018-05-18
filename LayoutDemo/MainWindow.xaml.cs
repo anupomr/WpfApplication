75a9a62175c12d78d9fbb2e07da9a5ed38fb56dd
Anupom Roy <anupomr@yahoo.com>
2018-05-18 15:26:57 -0400

    Layout is created

--- LayoutDemo/MainWindow.xaml.cs
+++ LayoutDemo/MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace LayoutDemo
         {
             InitializeComponent();
         }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+
+            switch (button.Name)
+            {
+                case "btnCanvas":
+                    frmMainArera.Content = new PgCAnvas();
+                    break;
+                case "btnDockPanel":
+                    frmMainArera.Content = new PgDockPanel();
+                    break;
+                case "PgGrid":
+                    frmMainArera.Content = new PgGrid();
+                    break;
+                case "PgStakePanel":
+                    frmMainArera.Content = new PgStakePanel();
+                    break;
+                case "PgTextControl":
+                    frmMainArera.Content = new PgTextControl();
+                    break;
+                case "btnUnknown":
+                    frmMainArera.Content = new PgUniformGrid();
+                    break;
+                case "btnWrap":
+                    frmMainArera.Content = new PgWrapPanel();
+                    break;
+            }
+
+
+        }
     }
 }
