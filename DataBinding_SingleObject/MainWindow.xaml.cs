eb3424631b4373d638f6a6a7166fff0b2c294413
Anupom Roy <anupomr@yahoo.com>
2018-05-24 19:41:51 -0400

    Data binding is created

--- DataBinding_SingleObject/MainWindow.xaml.cs
+++ DataBinding_SingleObject/MainWindow.xaml.cs
@@ -24,6 +24,20 @@ namespace DataBinding_SingleObject
         public MainWindow()
         {
             InitializeComponent();
+            medal = new Medal() { Color = MedalColor.Gold, Athlete = "Hao Lac", IsRecord = true, Year = 2008, TheEvent = "100 meter hurdles" };
+            DataContext = this;
+        }
+        private void btnChangeColor(object sender, RoutedEventArgs e)
+        {
+            medal.Color = MedalColor.Bronze;
+        }
+        private void btnChangeName(object sender, RoutedEventArgs e)
+        {
+            medal.Athlete = "Narendra";
+        }
+        private void btnShowObject(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(medal.ToString(), "Medal", MessageBoxButton.OK);
         }
     }
 }
