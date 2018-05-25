313ec74830d73f9ea9fe43f9a94376f116beb706
Anupom Roy <anupomr@yahoo.com>
2018-05-25 15:00:39 -0400

    Wolkthrough Lab Created

---
+++ RajAnuExpenceIT/pgExpenceItHome.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RajAnuExpenceIT
+{
+    /// <summary>
+    /// Interaction logic for pgExpenceItHome.xaml
+    /// </summary>
+    public partial class pgExpenceItHome : Page
+    {
+        public pgExpenceItHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
